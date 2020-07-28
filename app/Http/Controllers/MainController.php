160d4115fbe9e031103a24e21c702198e5783849
Will Thompson <williamjamesthompson@gmail.com>
2020-07-28 13:22:24 -0500

    One more test

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
+use Redis;
 use GuzzleHttp\Psr7;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Client;
